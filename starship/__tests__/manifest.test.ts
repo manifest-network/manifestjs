03e8d9fc5489054dbb356fd8899b288c2a8d4b59
Felix C. Morency <1102868+fmorency@users.noreply.github.com>
2024-09-06 14:06:41 -0400

    test: finish manifest module tests

--- starship/__tests__/manifest.test.ts
+++ starship/__tests__/manifest.test.ts
@@ -2,8 +2,14 @@ import "./setup.test";
 
 import { generateMnemonic, useChain } from "starshipjs";
 import { DirectSecp256k1HdWallet } from "@cosmjs/proto-signing";
-import { assertIsDeliverTxSuccess } from "@cosmjs/stargate";
-import { MsgPayout } from "../../src/codegen/liftedinit/manifest/v1/tx";
+import {
+  assertIsDeliverTxSuccess,
+  SigningStargateClient,
+} from "@cosmjs/stargate";
+import {
+  MsgBurnHeldBalance,
+  MsgPayout,
+} from "../../src/codegen/liftedinit/manifest/v1/tx";
 import { getSigningLiftedinitClient } from "../../src";
 
 // This is the POA_ADMIN_ADDRESS mnemonic as defined in the config.yaml file
@@ -11,7 +17,7 @@ const poaAdminMnemonic =
   "razor dog gown public private couple ecology paper flee connect local robot diamond stay rude join sound win ribbon soup kidney glass robot vehicle";
 
 describe("manifest module", () => {
-  let wallet, denom, address;
+  let wallet, denom, address, address2;
   let chainInfo, getCoin, getRpcEndpoint, creditFromFaucet;
 
   beforeAll(async () => {
@@ -26,50 +32,47 @@ describe("manifest module", () => {
     });
     address = (await wallet.getAccounts())[0].address;
 
-    await creditFromFaucet(address);
-  });
-
-  test("mint tokens", async () => {
     const wallet2 = await DirectSecp256k1HdWallet.fromMnemonic(
       generateMnemonic(),
       { prefix: chainInfo.chain.bech32_prefix }
     );
-    const address2 = (await wallet2.getAccounts())[0].address;
+    address2 = (await wallet2.getAccounts())[0].address;
 
-    const signingClient = await getSigningLiftedinitClient({
+    await creditFromFaucet(address);
+    await creditFromFaucet(address2);
+  });
+
+  const getFee = () => ({
+    amount: [{ denom, amount: "100000" }],
+    gas: "550000",
+  });
+
+  const getSigningClient = async () => {
+    return await getSigningLiftedinitClient({
       rpcEndpoint: await getRpcEndpoint(),
       signer: wallet,
     });
+  };
 
-    const fee = {
-      amount: [
-        {
-          denom,
-          amount: "100000",
-        },
-      ],
-      gas: "550000",
-    };
+  const checkBalance = async (
+    client: SigningStargateClient,
+    addr: string,
+    expectedAmount: number | bigint
+  ) => {
+    const balance = await client.getBalance(addr, denom);
+    expect(balance.amount).toEqual(expectedAmount.toString());
+    expect(balance.denom).toEqual(denom);
+  };
 
-    const initialAmount = 1;
+  test("mint tokens to destination", async () => {
+    const signingClient = await getSigningClient();
+    const fee = getFee();
+    const initialAmount = 1000000000000;
     const mintAmount = 1000000;
     const resultBalance = initialAmount + mintAmount;
 
-    // Send tokens to address2 so it exists in the ledger
-    await signingClient.sendTokens(
-      address,
-      address2,
-      [{ amount: initialAmount.toString(), denom }],
-      fee,
-      "send 1 token to address2"
-    );
+    await checkBalance(signingClient, address2, initialAmount);
 
-    // Get the current balance
-    const balance = await signingClient.getBalance(address2, denom);
-    expect(balance.amount).toEqual(initialAmount.toString());
-    expect(balance.denom).toEqual(denom);
-
-    // Mint tokens
     const payoutMsg = {
       typeUrl: MsgPayout.typeUrl,
       value: MsgPayout.fromPartial({
@@ -86,9 +89,32 @@ describe("manifest module", () => {
     );
 
     assertIsDeliverTxSuccess(resp);
+    await checkBalance(signingClient, address2, resultBalance);
+  });
 
-    const balance2 = await signingClient.getBalance(address2, denom);
-    expect(balance2.amount).toBe(resultBalance.toString());
-    expect(balance2.denom).toBe(denom);
+  test("burn tokens from self", async () => {
+    const signingClient = await getSigningClient();
+    const fee = getFee();
+    const burnAmount = 1000000;
+
+    const balance = await signingClient.getBalance(address, denom);
+    expect(balance.denom).toEqual(denom);
+
+    const resultBalance =
+      BigInt(balance.amount) -
+      BigInt(burnAmount) -
+      BigInt(fee.amount[0].amount);
+
+    const burnMsg = {
+      typeUrl: MsgBurnHeldBalance.typeUrl,
+      value: MsgBurnHeldBalance.fromPartial({
+        authority: address,
+        burnCoins: [{ denom, amount: burnAmount.toString() }],
+      }),
+    };
+    const resp = await signingClient.signAndBroadcast(address, [burnMsg], fee);
+
+    assertIsDeliverTxSuccess(resp);
+    await checkBalance(signingClient, address, resultBalance);
   });
 });
